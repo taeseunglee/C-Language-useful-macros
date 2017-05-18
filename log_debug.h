6ecb53171ac692484da0842a39b8b5f06d5aa1ce
taeseunglee <taeseunglee159@gmail.com>
2017-05-18 22:29:22 +0900

    Add a header that can print lines for log or debug

---
+++ log_debug.h
@@ -0,0 +1,27 @@
+#ifndef __LOG_DEBUG_H__
+#define __LOG_DEBUG_H__
+
+
+/* When the C++ compiler is in use, __cplusplus is defined */
+#ifdef __cplusplus
+#define FUNC __PRETTY_FUNCTION__
+#else
+#define FUNC __FUNCTION__
+#endif /* __cplusplus */
+
+
+#ifdef  DEBUG
+/* file:line:function: */
+#define LOG_DEBUG(fmt, ...) printf("%s:%d:%s: " fmt, \
+        __FILE__, __LINE__, FUNC, ##__VA_ARGS__)
+#elif   LOG
+#define LOG_DEBUG(fmt, ...) printf("(%s) %s:%d:%s: " fmt, \
+        __TIME__, __FILE__, __LINE__, FUNC, ##__VA_ARGS__)
+#elif   LOG_DATE
+#define LOG_DEBUG(fmt, ...) printf("(%s, %s) %s:%d:%s: " fmt, \
+        __DATE__, __TIME__, __FILE__, __LINE__, FUNC, ##__VA_ARGS__)
+#else
+#define LOG_DEBUG(fmt, ...)
+#endif /* DEBUG */
+
+#endif /* __LOG_DEBUG_H__ */
