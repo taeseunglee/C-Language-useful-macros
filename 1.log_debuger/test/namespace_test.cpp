1c6d82c921c538d223858a01217f51614e3d98ad
taeseunglee <taeseunglee159@gmail.com>
2017-05-28 22:06:26 +0900

    Add an cpp test file with namespace

--- 1.log_debuger/test/namespace_test.cpp
+++ 1.log_debuger/test/namespace_test.cpp
@@ -1,8 +1,34 @@
+/* Code Reference:
+ * https://www.tutorialspoint.com/cplusplus/cpp_namespaces.htm
+ * */
 #include <iostream>
+#include "../log_debug.h"
+using namespace std;
 
-int main ()
-{
-  int a;
-  printf("");
-
+// first name space
+namespace first_space{
+   void func(){
+      cout << "Inside first_space" << endl;
+      LOG_DEBUG("Hello Wolrd first\n");
+   }
 }
+
+// second name space
+namespace second_space{
+   void func(){
+      cout << "Inside second_space" << endl;
+      LOG_DEBUG("Hello Wolrd second\n");
+   }
+}
+
+int main () {
+ 
+   // Calls function from first name space.
+   first_space::func();
+   
+   // Calls function from second name space.
+   second_space::func(); 
+
+   return 0;
+}
+
